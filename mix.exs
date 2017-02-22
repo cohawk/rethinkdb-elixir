7c16364ac24de6040aa8640543821b31368516b7
COHAWK <cohawk@users.noreply.github.com>
2017-02-22 18:06:08 -0500

    Relax poison dependancy version

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule RethinkDB.Mixfile do use Mix.Project
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:poison, "~>3.1.0"},
+      {:poison, "~>3.0"},
       {:earmark, "~> 1.1", only: :dev},
       {:ex_doc, "~> 0.15.0", only: :dev},
       {:flaky_connection, github: "hamiltop/flaky_connection", only: :test},
